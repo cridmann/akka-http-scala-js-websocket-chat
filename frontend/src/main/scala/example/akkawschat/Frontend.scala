dae7380085d33e86162de6c288c367a8e6da4137
Chris Ridmann <chris.ridmann@gmail.com>
2015-10-11 18:47:12 -0400

    very basic integration with scalajs-reactjs

--- frontend/src/main/scala/example/akkawschat/Frontend.scala
+++ frontend/src/main/scala/example/akkawschat/Frontend.scala
@@ -1,83 +1,107 @@
 package example.akkawschat
 
 import org.scalajs.dom.raw._
-
 import scala.scalajs.js
 import org.scalajs.dom
-
 import upickle._
 import shared.Protocol._
+import japgolly.scalajs.react.vdom.prefix_<^._
+import japgolly.scalajs.react._
 
 object Frontend extends js.JSApp {
-  val joinButton = dom.document.getElementById("join").asInstanceOf[HTMLButtonElement]
-  val sendButton = dom.document.getElementById("send").asInstanceOf[HTMLButtonElement]
+
+  // just a dumb websocket client
+  class SocketService(name: String) {
+    private def getWebsocketUri(document: Document, nameOfChatParticipant: String): String = {
+      val wsProtocol = if (dom.document.location.protocol == "https:") "wss" else "ws"
+
+      s"$wsProtocol://${dom.document.location.host}/chat?name=$nameOfChatParticipant"
+    }
+
+    private val client = new WebSocket(getWebsocketUri(dom.document, name))
+
+    def listener(f: MessageEvent ⇒ Unit) =
+      client.onmessage = { (event: MessageEvent) ⇒ f(event) }
+
+    def sendMessage(m2send: String): Unit = client.send(m2send)
+
+    def close(): Unit = client.close()
+  }
+
+  case class State(name: String, messageToSend: String, messages: List[ChatMessage], socket: Option[SocketService])
+
+  class Backend($: BackendScope[Unit, State]) {
+    def handleSubmit(e: ReactEventI) = {
+      $.modState(_.copy(name = e.target.value))
+    }
+
+    def onNameChange(e: ReactEventI) =
+      $.modState(_.copy(name = e.target.value))
+
+    def onMessageChange(e: ReactEventI) =
+      $.modState(_.copy(messageToSend = e.target.value))
+
+    def sendMessage(e: ReactEventI) = {
+      e.preventDefault()
+      $.get().socket foreach { ws ⇒
+        ws.sendMessage($.get().messageToSend)
+      }
+      $.modState(_.copy(messageToSend = ""))
+    }
+
+    def joinChat(e: ReactEventI) = {
+      e.preventDefault()
+      val socket = new SocketService($.get().name)
+      $.modState(_.copy(socket = Some(socket)))
+      // todo - attach to reactjs 'EventListener' api
+      socket.listener(event ⇒ {
+        val wsMsg = read[ChatMessage](event.data.toString)
+        $.modState(s ⇒ s.copy(messages = s.messages :+ wsMsg))
+      })
+    }
+
+    def isJoined = $.get().socket.isDefined
+  }
+
+  val JoinChat =
+    ReactComponentB[(State, Backend)]("ChatName")
+      .render(P ⇒ {
+        val (s, b) = P
+        <.form(^.onSubmit ==> b.joinChat,
+          <.input(^.placeholder := "Who?", ^.onChange ==> b.onNameChange, ^.value := s.name),
+          <.button("Join Chat"))
+      })
+      .build
+
+  val SendMessage =
+    ReactComponentB[(State, Backend)]("SendMessage")
+      .render(P ⇒ {
+        val (s, b) = P
+        <.form(^.onSubmit ==> b.sendMessage,
+          <.input(^.placeholder := "What?", ^.onChange ==> b.onMessageChange, ^.value := s.messageToSend),
+          <.button("Send Message"))
+      }).build
+
+  val ConversationHistory =
+    ReactComponentB[List[ChatMessage]]("ConversationHistory")
+      .render(props ⇒ {
+        def showMessage(message: ChatMessage) = <.li(s"[${message.sender}] - ${message.message}")
+        <.ul(props map showMessage)
+      }).build
+
+  val Chat =
+    ReactComponentB[Unit]("Chat")
+      .initialState(State("", "", List(ChatMessage("admin", "Not connected yet")), None))
+      .backend(new Backend(_))
+      .render((_, S, B) ⇒ {
+        <.div(
+          JoinChat((S, B)),
+          SendMessage((S, B)),
+          ConversationHistory(S.messages))
+      }).build
 
   def main(): Unit = {
-    val nameField = dom.document.getElementById("name").asInstanceOf[HTMLInputElement]
-    joinButton.onclick = { (event: MouseEvent) ⇒
-      joinChat(nameField.value)
-      event.preventDefault()
-    }
-    nameField.focus()
-    nameField.onkeypress = { (event: KeyboardEvent) ⇒
-      if (event.keyCode == 13) {
-        joinButton.click()
-        event.preventDefault()
-      }
-    }
-  }
-
-  def joinChat(name: String): Unit = {
-    joinButton.disabled = true
-    val playground = dom.document.getElementById("playground")
-    playground.innerHTML = s"Trying to join chat as '$name'..."
-    val chat = new WebSocket(getWebsocketUri(dom.document, name))
-    chat.onopen = { (event: Event) ⇒
-      playground.insertBefore(p("Chat connection was successful!"), playground.firstChild)
-      sendButton.disabled = false
-
-      val messageField = dom.document.getElementById("message").asInstanceOf[HTMLInputElement]
-      messageField.focus()
-      messageField.onkeypress = { (event: KeyboardEvent) ⇒
-        if (event.keyCode == 13) {
-          sendButton.click()
-          event.preventDefault()
-        }
-      }
-      sendButton.onclick = { (event: Event) ⇒
-        chat.send(messageField.value)
-        messageField.value = ""
-        messageField.focus()
-        event.preventDefault()
-      }
-
-      event
-    }
-    chat.onerror = { (event: ErrorEvent) ⇒
-      playground.insertBefore(p(s"Failed: code: ${event.colno}"), playground.firstChild)
-      joinButton.disabled = false
-      sendButton.disabled = true
-    }
-    chat.onmessage = { (event: MessageEvent) ⇒
-      val wsMsg = read[ChatMessage](event.data.toString)
-      playground.insertBefore(p(s"${wsMsg.sender} said: ${wsMsg.message}"), playground.firstChild)
-    }
-    chat.onclose = { (event: Event) ⇒
-      playground.insertBefore(p("Connection to chat lost. You can try to rejoin manually."), playground.firstChild)
-      joinButton.disabled = false
-      sendButton.disabled = true
-    }
-  }
-
-  def getWebsocketUri(document: Document, nameOfChatParticipant: String): String = {
-    val wsProtocol = if (dom.document.location.protocol == "https:") "wss" else "ws"
-
-    s"$wsProtocol://${dom.document.location.host}/chat?name=$nameOfChatParticipant"
-  }
-
-  def p(msg: String) = {
-    val paragraph = dom.document.createElement("p")
-    paragraph.innerHTML = msg
-    paragraph
+    val reactground = dom.document.getElementById("reactground")
+    React.render(Chat(Unit), reactground)
   }
 }
--- project/ChatBuild.scala
+++ project/ChatBuild.scala
@@ -18,14 +18,22 @@ object ChatBuild extends Build {
       .enablePlugins(ScalaJSPlugin)
       .settings(commonSettings: _*)
       .settings(
+        emitSourceMaps := false,
         persistLauncher in Compile := true,
         persistLauncher in Test := false,
+        jsDependencies ++= Seq(
+          "org.webjars" % "react" % "0.12.2" / "react-with-addons.js" commonJSName "React" minified "react-with-addons.min.js"
+        ),
         testFrameworks += new TestFramework("utest.runner.Framework"),
         libraryDependencies ++= Seq(
           "org.scala-js" %%% "scalajs-dom" % "0.8.0",
           "com.lihaoyi" %%% "upickle" % "0.2.8",
-          "com.lihaoyi" %%% "utest" % "0.3.0" % "test"
+          "com.lihaoyi" %%% "utest" % "0.3.0" % "test",
+          "com.github.chandu0101.scalajs-react-components" %%% "core" % "0.1.0",
+          "com.github.japgolly.scalajs-react" %%% "core" % "0.9.2",
+          "com.github.japgolly.scalajs-react" %%% "extra" % "0.9.2"
         )
+
       )
       .dependsOn(sharedJs)
 
